2d1cc7c51471e713a5ca0d69c3f8dc185fc4f945
mdziub <mackowiecki@o2.pl>
2016-11-22 00:26:17 +0100

    model filled

--- src/main/java/Modele/Entity/ModelEntity.java
+++ src/main/java/Modele/Entity/ModelEntity.java
@@ -1,7 +1,43 @@
 package Modele.Entity;
 
-/**
- * Created by macko_000 on 2016-11-21.
- */
+
 public class ModelEntity {
+    private Long id_modele;
+    private String nazwaMod;
+    private String producent;
+    private String program;
+
+    public Long getId_modele() {
+        return id_modele;
+    }
+
+    public void setId_modele(Long id_modele) {
+        this.id_modele = id_modele;
+    }
+
+    public String getNazwaMod() {
+        return nazwaMod;
+    }
+
+    public void setNazwaMod(String nazwaMod) {
+        this.nazwaMod = nazwaMod;
+    }
+
+    public String getProducent() {
+        return producent;
+    }
+
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+
 }
