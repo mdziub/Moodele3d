70c9078ab23babf1aa2caba260a3f1397021ae62
mdziub <mackowiecki@o2.pl>
2016-11-21 20:50:47 +0100

    ServiceTest added

---
+++ src/test/java/Modele/Service/ServiceTest.java
@@ -0,0 +1,29 @@
+package Modele.Service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by macko_000 on 2016-11-21.
+ */
+public class ServiceTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    @Test
+    public void dupa() throws Exception {
+        assertEquals(true,true);
+
+    }
+}
