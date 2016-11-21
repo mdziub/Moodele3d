5c5509e3ca09b6b188073308c658ae71dc953c78
mdziub <mackowiecki@o2.pl>
2016-11-21 20:29:42 +0100

    Modele package added

---
+++ src/main/java/Modele/Entity/ModelEntity.java
@@ -0,0 +1,7 @@
+package Modele.Entity;
+
+/**
+ * Created by macko_000 on 2016-11-21.
+ */
+public class ModelEntity {
+}
---
+++ src/main/java/Modele/Service/Service.java
@@ -0,0 +1,11 @@
+package Modele.Service;
+
+import Modele.Entity.CategoryEntity;
+import Modele.Entity.ModelEntity;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+}
