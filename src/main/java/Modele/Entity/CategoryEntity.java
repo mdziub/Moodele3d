b2f69663738b01a03f4d6affd4d32e4f7d0d4324
mdziub <mackowiecki@o2.pl>
2016-11-21 23:47:07 +0100

    fill category

--- src/main/java/Modele/Entity/CategoryEntity.java
+++ src/main/java/Modele/Entity/CategoryEntity.java
@@ -1,7 +1,53 @@
 package Modele.Entity;
 
-/**
- * Created by macko_000 on 2016-11-21.
- */
+
 public class CategoryEntity {
+
+    private Long id_kategoria;
+    private String nazwaKat;
+    private String opis;
+    private int numer;
+    private Long idmod;
+
+    public Long getId_kategoria() {
+        return id_kategoria;
+    }
+
+    public void setId_kategoria(Long id_kategoria) {
+        this.id_kategoria = id_kategoria;
+    }
+
+    public String getNazwaKat() {
+        return nazwaKat;
+    }
+
+    public void setNazwaKat(String nazwaKat) {
+        this.nazwaKat = nazwaKat;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public Long getIdmod() {
+        return idmod;
+    }
+
+    public void setIdmod(Long idmod) {
+        this.idmod = idmod;
+    }
+
+
 }
--- src/main/java/Modele/Service/Service.java
+++ src/main/java/Modele/Service/Service.java
@@ -58,5 +58,7 @@ public class Service{
         }
 
         }
+
+
     }
 }
